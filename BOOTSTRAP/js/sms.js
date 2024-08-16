1f1c61ac1f74501eb99bac4c97aaab8d15236582
AvinashGangary <avinashgangary78@gmail.com>
2024-08-16 11:43:40 +0530

    Delete BOOTSTRAP directory

--- BOOTSTRAP/js/sms.js
+++
@@ -1,8 +0,0 @@
-const counter = document.getElementById('counter');
-const textbox = document.getElementById('textbox');
-const maxLength = 100;
-
-textbox.addEventListener('input', () => {
-    const remaining = maxLength - textbox.value.length;
-    counter.textContent = remaining;
-});
